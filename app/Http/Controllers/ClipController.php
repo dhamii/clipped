5a9c0fbcbed5aac864b540bdae48d5d890a8a8b1
dhamii <teddydamivee@gmail.com>
2025-06-06 16:35:11 +0100

    Message

--- app/Http/Controllers/ClipController.php
+++ app/Http/Controllers/ClipController.php
@@ -10,19 +10,15 @@ class ClipController extends Controller
     public function index(Request $request, $clipurl){
         $clipurl = $request->clipurl;
 
-        // dd($clipurl);
         $store = Clip::create([
             'clip' => $clipurl
         ]);
 
         $clip = $store->clip;
-        // dd($clip);
         return redirect('/li/' . $clip);
     }
 
     public function viewCreate(){
-        // $clipurl = $request->clipurl;
-        // dd($clipurl);
         return view('createclip');
     }
         public function viewContent($clip){
@@ -31,11 +27,8 @@ class ClipController extends Controller
         }
 
         public function saveContent(Request $request, $clip){
-            // dd($clip);
             $textcontent = $request->clipcontent;
             $clip = Clip::where('clip', $clip)->update(['clipcontent' => $textcontent]);
-
-            // dd($clip);
             return 'created';
         }
         //
